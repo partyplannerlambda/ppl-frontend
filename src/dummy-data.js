26d74d5b552f2e6c0f5b1a50e69ea01d4ea1a5e3
KerstingJ <Kersting.Josh@Gmail.com>
2019-04-16 14:54:46 -0400

    framed out todo and shopping list, showing items, added forms

--- src/dummy-data.js
+++ src/dummy-data.js
@@ -24,3 +24,54 @@ export default [
     date: "2019-07-16"
   }
 ];
+
+
+export const todos = [
+  {
+    id: 1,
+    text: "do a thing",
+    completed: false
+  },
+  {
+    id: 2,
+    text: "do another thing",
+    completed: false
+  },
+  {
+    id: 3,
+    text: "dont do a thing",
+    completed: false
+  },
+  {
+    id: 4,
+    text: "do a thangthang",
+    completed: false
+  }
+];
+
+export const shopping = [
+  {
+    id: 1,
+    text: "buy a thing",
+    completed: false,
+    price: null
+  },
+  {
+    id: 2,
+    text: "buy another thing",
+    completed: false,
+    price: null
+  },
+  {
+    id: 3,
+    text: "buynt a thing",
+    completed: false,
+    price: null
+  },
+  {
+    id: 4,
+    text: "buy a thangthang",
+    completed: false,
+    price: null
+  }
+]
--- src/views/EventView.js
+++ src/views/EventView.js
@@ -5,6 +5,8 @@ import {connect} from 'react-redux';
 import Header from '../components/Header'
 import EventCard from '../components/EventCard'
 import EventMoodModal from '../components/EventMoodModal'
+import TodoList from '../components/TodoList'
+import ShoppingList from '../components/ShoppingList'
 
 import data from '../dummy-data';
 
@@ -12,14 +14,18 @@ export default function(props){
     const [event, setEvent] = useState(undefined);
 
     useEffect(()=>{
-        setEvent(data.find(event => event.id == props.match.params.id))
+        setEvent(data.find(event => event.id+"" === props.match.params.id+""))
     }, [])
 
     console.log(event)
     return (<> 
         <Header />
         <EventCard mainPage event={event}/>
-        <EventMoodModal />
+        <EventMoodModal event={event}/>
+        <div className="listContainer">
+            <TodoList event={event} />
+            <ShoppingList event={event} />
+        </div>
     </>)
 }
 
