9c0bf92dfadf878d028489555827a1a06ecec7af
KerstingJ <Kersting.Josh@Gmail.com>
2019-04-16 11:52:52 -0400

    built out basic event card

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -1,6 +1,18 @@
 import React from 'react'
 
 export default function(props){
+    const {event} = props;
+    if (!event) {
+        return <div>It Seems this event doesn't exist</div>
+    }
 
-    return(<div>This is where id keep an Event to quickly preview it</div>)
+    return(
+        <div>
+            <h2>{event.name}</h2>
+            <p><strong>Theme: </strong>{event.theme}</p>
+            <p><strong>Guests: </strong>{event.n_of_guests}</p>
+            <p><strong>Budget: </strong>{event.budget}</p>
+            <p><strong>Date: </strong>{event.date}</p>
+        </div>
+    )
 }
