ae6bebbbc6185fff96a72bb930637ed25981d135
KerstingJ <Kersting.Josh@Gmail.com>
2019-04-17 11:43:56 -0400

    added navlink to login

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,6 +13,7 @@ export default withRouter(function(props){
             <h1 onClick={pushHome}>Party Planner</h1>
             <nav>
                 <NavLink exact to="/">Home</NavLink>
+                <NavLink to="/login">Login</Navlink>
             </nav>
         </Header>
     )
