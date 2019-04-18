02bd08944f70bc06c56ffa8e7e4f57b8ef740873
KerstingJ <Kersting.Josh@Gmail.com>
2019-04-18 19:58:38 -0400

    added key

--- src/components/EventMoodModal.js
+++ src/components/EventMoodModal.js
@@ -2,29 +2,29 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function(props) {
-//   const { party } = props;
-//   let [isLoading, setIsLoading] = useState(false);
+  const { party } = props;
+  let [isLoading, setIsLoading] = useState(false);
 
-//   useEffect(() => {
-//     axios
-//       .get(
-//         `https://unsplash.com/search/photos/?query=${encodeURIComponent(party.theme)}`,
-//         { //config optional arg
-//           headers: {
-//             "Content-Type": "application/json",
-//             Authorization:
-//               `Client-ID ${UNSPLASH_API_KEY}`
-//           }
-//         }
-//       )
-//       .then(res => {
-//         console.log("This is in the Image Modal", res);
+  useEffect(() => {
+    axios
+      .get(
+        `https://unsplash.com/search/photos/?query=${encodeURIComponent(party.theme)}`,
+        { //config optional arg
+          headers: {
+            "Content-Type": "application/json",
+            Authorization:
+              `Client-ID 0b0cf775d52dc76301a5a814a41adf0d5159b2160989a1ee98b07ea3e086d28a`
+          }
+        }
+      )
+      .then(res => {
+        console.log("This is in the Image Modal", res);
         
-//       })
-//       .catch(err => {
-//         console.log("This is in the Image Modal", err);
-//       });
-//   }, []);
+      })
+      .catch(err => {
+        console.log("This is in the Image Modal", err);
+      });
+  }, []);
 
   
   let imgUrl = "https://picsum.photos/1800/700/?random";
